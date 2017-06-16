c098ba17b437365cf3a8d49e26cb6d291d495ead
ColinKnecht <cknecht87@gmail.com>
2017-06-16 13:39:27 -0400

    Before Video 163
    Adding searchable xml file

--- FlickrBrowser/app/src/main/java/com/colinknecht/flickrbrowserapp/SearchActivity.java
+++ FlickrBrowser/app/src/main/java/com/colinknecht/flickrbrowserapp/SearchActivity.java
@@ -5,10 +5,12 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
+import android.widget.SearchView;
 
 public class SearchActivity extends BaseActivity {
-
+    private SearchView mSearchView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,4 +18,9 @@ public class SearchActivity extends BaseActivity {
         activateToolbar(true);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+        return true;
+    }
 }
